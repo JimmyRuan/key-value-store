5dc3159d77fc68b1be5f2d13a31f556da463fc44
Jimmy Ruan <jimmyruan12@gmail.com>
2021-09-16 16:41:28 -0400

    fix the tests

--- ruby/memory_key_value_store/memory_key_value_store.rb
+++ ruby/memory_key_value_store/memory_key_value_store.rb
@@ -1,13 +1,24 @@
 class MemoryKeyValueStore
-  def initialize
+  def initialize(values)
+    @values = values
   end
 
-  def get
+  def get(key)
+    @values[key]
   end
 
-  def set
+  def set(key, value)
+    unless value.is_a? String
+      raise "The value has to be string"
+    end
+    @values[key] = value
+    'OK'
   end
 
-  def keys
+  def keys(pattern=nil)
+    unless pattern
+      return @values.keys
+    end
+    @values.keys.select{|key| key.to_s[/#{pattern}/]}
   end
 end
--- ruby/memory_key_value_store/spec/memory_key_value_store_spec.rb
+++ ruby/memory_key_value_store/spec/memory_key_value_store_spec.rb
@@ -4,37 +4,68 @@ require_relative '../memory_key_value_store'
 describe "MemoryKeyValueStore" do
   describe "#get" do
     context "with a matching key" do
-      it "gets the value of key"
+      it "gets the value of key" do
+        store = MemoryKeyValueStore.new({aaa: 'red', bbb: 'yellow'})
+        expect(store.get(:aaa)).to eq('red')
+      end
     end
 
     context "without a matching key" do
-      it "returns the special value nil"
+      it "returns the special value nil" do
+        store = MemoryKeyValueStore.new({aaa: 'red', bbb: 'yellow'})
+        expect(store.get(:ccc)).to eq(nil)
+      end
     end
 
     context "with a non-string value" do
-      it "returns an error"
+      it "returns an error" do
+        store = MemoryKeyValueStore.new({aaa: 'red', bbb: 'yellow'})
+        expect{store.set(:ccc, :string)}.to raise_error(RuntimeError)
+      end
     end
   end
 
   describe "#set" do
     context "when they key does not hold a value" do
-      it "sets the key to hold the string value"
-      it "returns the string 'OK'"
+        it  "sets the key to hold the string value" do
+          store = MemoryKeyValueStore.new({aaa: 'red', bbb: 'yellow'})
+          store.set(:ccc, 'orange')
+          expect(store.get(:ccc)).to eq 'orange'
+        end
+
+        it "returns the string 'OK'" do
+          store = MemoryKeyValueStore.new({aaa: 'red', bbb: 'yellow'})
+          expect(store.set(:ccc, 'orange')).to eq 'OK'
+        end
     end
 
     context "when they key already holds a value" do
-      it "sets the key to hold the string value"
-      it "returns the string 'OK'"
+      it  "sets the key to hold the string value" do
+        store = MemoryKeyValueStore.new({ccc: 'orange', bbb: 'yellow'})
+        store.set(:ccc, 'banana')
+        expect(store.get(:ccc)).to eq 'banana'
+      end
+
+      it "returns the string 'OK'" do
+        store = MemoryKeyValueStore.new({aaa: 'red', bbb: 'yellow'})
+        expect(store.set(:aaa, 'orange')).to eq 'OK'
+      end
     end
   end
 
   describe "#keys" do
     context "when no regex pattern is passed" do
-      it "returns all keys"
+      it "returns all keys" do
+        store = MemoryKeyValueStore.new({aaa: 'red', bbb: 'yellow'})
+        expect(store.keys).to eq [:aaa, :bbb]
+      end
     end
 
     context "when a regex pattern is passed" do
-      it "returns all keys matching regex pattern"
+      it "returns all keys matching regex pattern" do
+        store = MemoryKeyValueStore.new({aaa: 'red', bbb: 'yellow'})
+        expect(store.keys('aa')).to eq [:aaa]
+      end
     end
   end
 end
